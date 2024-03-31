107a5c594f84e4a90ff40f47fc58e8ff67135ad9
Camila Nahuel <jayabookwalter@Jayas-MacBook-Air.local>
2024-03-31 11:43:25 -0500

    reto3

--- app/src/main/java/introduccion/Reto3/Subasta.kt
+++ app/src/main/java/introduccion/Reto3/Subasta.kt
@@ -7,41 +7,42 @@ class Subasta (val articles: List<Articulo>){
         get() = field
 
     fun recibirOferta(article: Articulo, oferente: String, price: Int) {
-        offers[article]!!.add(Oferta(oferente, price))
+        val ofertasArticulo:MutableList<Oferta> = mutableListOf()
+        ofertasArticulo.add(Oferta(oferente, price))
+        offers[article] = ofertasArticulo
     }
 
     fun determinarGanador() {
         for (articulo in articles) {
             val ofertasArticulo = offers[articulo]
             if (ofertasArticulo == null || ofertasArticulo.isEmpty()) {
-                println("El artículo '$articulo' no ha recibido ofertas y se vende a la casa de subastas por ${articulo.precioBase}€.")
+                println("El artículo '$articulo' no ha recibido ofertas y se vende a la casa de subastas por ${articulo.basePrice}€.")
                 continue
             }
             val ganador = ofertasArticulo.maxByOrNull { it.precio }!!
-            println("El artículo '$articulo' ha sido vendido a ${ganador.oferente} por ${ganador.precio}€.")
+            println("El artículo ${articulo.nombre} ha sido vendido a ${ganador.oferente} por ${ganador.precio}€.")
         }
 
     }
 }
 
 fun main(){
-    val articulos = listOf(
-        Articulo("Cuadro", 100),
-        Articulo("Escultura", 200),
-        Articulo("Jarrón", 50),
-    )
+    val a1 = Articulo("Cuadro", 100)
+    val a2 =Articulo("Escultura", 200)
+    val a3 =Articulo("Jarrón", 50)
+    val articulos = listOf(a1,a2,a3)
 
     val subasta = Subasta(articulos)
 
 // Ofertas por el cuadro
-    subasta.recibirOferta("Cuadro", "Juan", 120)
-    subasta.recibirOferta("Cuadro", "María", 150)
+    subasta.recibirOferta(a1, "Juan", 120)
+    subasta.recibirOferta(a1, "María", 150)
 
 // Oferta por la escultura
-    subasta.recibirOferta("Escultura", "Pedro", 250)
+    subasta.recibirOferta(a2, "Pedro", 250)
 
 // Oferta por un artículo inexistente
-    subasta.recibirOferta("Jarrón", "Ana", 40)
+    subasta.recibirOferta(a3, "Ana", 40)
 
 // Determinar el ganador de cada subasta
     subasta.determinarGanador()
