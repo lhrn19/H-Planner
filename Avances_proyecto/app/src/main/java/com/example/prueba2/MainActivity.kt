c48d6af1fc3f0b411e434b551a7f95e87739369d
LauraaaH299 <laurava.hernandez@urosario.edu.co>
2024-04-06 21:25:56 -0500

    update de las views

--- Avances_proyecto/app/src/main/java/com/example/prueba2/MainActivity.kt
+++ Avances_proyecto/app/src/main/java/com/example/prueba2/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState) //create profile,change password,user_info
-        setContentView(R.layout.daily_view)
+        setContentView(R.layout.your_calendars)
     }
 }
