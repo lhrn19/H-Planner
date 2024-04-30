5eaeea7c6c2e4d37c661dbb839e6331cbb3d8103
sofiamaring18 <dannasofiamarin@gmail.com>
2024-04-30 09:39:35 -0500

    Base proyecto pipipi

---
+++ Avances_proyecto/app/src/main/java/com/example/prueba2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.prueba2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState) //create profile,change password,user_info
+        setContentView(R.layout.add_new_event)
+    }
+}
