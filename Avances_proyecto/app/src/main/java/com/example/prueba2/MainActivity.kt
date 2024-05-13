3e290022267ffa069446e7015d66bfbbe7e30a7d
sofiamaring18 <dannasofiamarin@gmail.com>
2024-05-13 09:19:31 -0500

    .

--- Avances_proyecto/app/src/main/java/com/example/prueba2/MainActivity.kt
+++ Avances_proyecto/app/src/main/java/com/example/prueba2/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.prueba2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import com.google.firebase.Firebase
+import com.google.firebase.database.database
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState) //create profile,change password,user_info
-        setContentView(R.layout.add_new_event)
+        setContentView(R.layout.login)
+        val database = Firebase.database
+        val myRef = database.getReference("Usuario_HiPlanner")
+
     }
+    fun create_account(view:View){
+        setContentView(R.layout.login)
+    }
+    fun sign_out(view:View){
+        setContentView(R.layout.login)
+    }
+    fun sign_in(view:View){
+        setContentView(R.layout.dashboard)
+    }
+    fun change_password(view:View){
+        setContentView(R.layout.login)
+    }
+
+    //no me acuerdo si el nombre de la función debe ser igual al de la clase en conexiones.kt
+
 }
+
---
+++ Avances_proyecto/app/src/main/java/com/example/prueba2/conexiones.kt
@@ -0,0 +1,96 @@
+package com.example.prueba2
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import android.widget.LinearLayout
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class create_account : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.new_account)
+        val boton_crear_cuenta = findViewById<Button>(R.id.create_account)
+        boton_crear_cuenta.setOnClickListener {
+            setContentView(R.layout.login)
+        }
+    }
+}
+
+class sign_out : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.profile_information)
+        val boton_sign_out = findViewById<Button>(R.id.sign_out)
+        boton_sign_out.setOnClickListener {
+            setContentView(R.layout.login)
+        }
+    }
+}
+
+class sign_in : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+        val boton_sign_in = findViewById<Button>(R.id.sign_in)
+        boton_sign_in.setOnClickListener {
+            setContentView(R.layout.dashboard)
+        }
+    }
+}
+
+class boton_change_password : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.change_password)
+        val boton_sing_in = findViewById<Button>(R.id.change_password)
+        boton_sing_in.setOnClickListener {
+            setContentView(R.layout.dashboard)
+        }
+    }
+}
+
+class sign_up : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+        val text_sign_up = findViewById<TextView>(R.id.sign_up)
+        text_sign_up.setOnClickListener {
+            setContentView(R.layout.new_account)
+        }
+    }
+}
+
+class text_change_password : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.profile_information)
+        val text_change_password = findViewById<TextView>(R.id.change_password)
+        text_change_password.setOnClickListener {
+            setContentView(R.layout.change_password)
+        }
+    }
+}
+
+class regresar_profile_info : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.profile_information)
+        val layout_regresar_profile_info = findViewById<LinearLayout>(R.id.regresar_profile_info)
+        layout_regresar_profile_info.setOnClickListener {
+            setContentView(R.layout.dashboard)
+        }
+    }
+}
+
+class regresar_change_password : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.change_password)
+        val layout_regresar_change_password = findViewById<LinearLayout>(R.id.regresar_change_password)
+        layout_regresar_change_password.setOnClickListener {
+            setContentView(R.layout.profile_information)
+        }
+    }
+}
+
